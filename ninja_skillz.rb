fade82c0ee9d3fb8231eda30dc2bfb39060ed5ac
paulpellom@gmail.com <paulpellom@gmail.com>
2018-03-22 14:22:47 -0400

    hw completed

---
+++ ninja_skillz.rb
@@ -0,0 +1,77 @@
+BOOLEAN CHALLENGES
+# @a = true 
+# @b = false
+
+# if @b == false
+#     @a = false
+# end
+
+
+# if @a != @b
+#     @a = @b
+# end
+
+
+# if @b != @a
+#     @b = @a
+# end 
+
+NIL CHALLENGES
+
+# @d = nil
+
+# @sentence =  "#{@d} was here"
+
+
+
+# @cody = "Hi I'm Cody"
+# @likesto = "likes to take long walks on the beach, kinda..."
+
+# @likesto = "#{@cody} #{@likesto}"
+
+# @g = '2'
+
+# @gtoNumber = 2.to_s
+
+
+# puts "Pick an integer" 
+# @numberpicked = gets.to_i
+
+# puts @numberpicked * 2
+
+# 
+
+
+# @myFirstArray = ['cody', 'jamie', 'blake', 'arial', 'cat', 1, 3, 5, 6, 7, 9]
+
+
+# @myFavoriteAnimal = {
+#     'cat': 'stardust',
+#     'snake': 'bob',
+#     'tiger': 'phillyboy',
+#     'bird': 'parrotman',
+#     'moose': 'dummyhead'
+# }
+
+
+HASHES SKILLZ
+
+# @myFavoriteMovie = {}
+
+# @firstRange = (1..10).to_a
+
+# @secondRange =(1..1000)
+
+# @firstRange.each do |number|
+    
+#     p number.to_s
+# end
+
+# @x = 0
+
+
+# while  @x < 50  do 
+#     p @x +=1 
+
+# end
+    
